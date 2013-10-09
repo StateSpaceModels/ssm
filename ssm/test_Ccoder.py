7ad612629f6a4be4a1ed316d3238ea6383cdc745
JDureau <dureau.joseph@gmail.com>
2013-10-09 23:01:50 +0200

    test_Ccoder models creation

--- ssm/test_Ccoder.py
+++ ssm/test_Ccoder.py
@@ -15,23 +15,22 @@ class TestCcoder(unittest.TestCase):
         self.m_diff = Cmodel(model)
 
         model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
-        model["resources"][1]["data"][2]["white_noise"] = {}
-        model["resources"][1]["data"][3]["white_noise"] = {}
+        del model["resources"][1]["data"][2]["white_noise"] 
+        del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][0]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][1]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         self.m_noise2 = Cmodel(model)
 
         model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
-        model["resources"][1]["data"][2]["white_noise"] = {}
-        model["resources"][1]["data"][3]["white_noise"] = {}
+        del model["resources"][1]["data"][2]["white_noise"] 
+        del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][4]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][5]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
-        print model["resources"][1]["data"][4]["white_noise"]
         self.m_noise3 = Cmodel(model)
 
         model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
-        model["resources"][1]["data"][2]["white_noise"] = {}
-        model["resources"][1]["data"][3]["white_noise"] = {}
+        del model["resources"][1]["data"][2]["white_noise"] 
+        del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][8]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][9]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         self.m_noise4 = Cmodel(model)
@@ -47,9 +46,8 @@ class TestCcoder(unittest.TestCase):
         self.m_noise7 = Cmodel(model)
 
         model = json.load(open(os.path.join('..' ,'diff', 'model', 'datapackage.json')))
-        model["resources"][1]["data"][10] = {"from": "R_paris",   "to": "I_paris",   "rate": "correct_rate(v)",            "description":"testing"}
-        model["resources"][1]["data"][11] = {"from": "R_nyc",   "to": "I_nyc",   "rate": "correct_rate(v)",                "description":"testing"}
-        print model["resources"][1]["data"][11]
+        model["resources"][1]["data"].append({"from": "R_paris",   "to": "I_paris",   "rate": "correct_rate(v)",            "description":"testing"})
+        model["resources"][1]["data"].append({"from": "R_nyc",   "to": "I_nyc",   "rate": "correct_rate(v)",                "description":"testing"})
         self.m_diff2 = Cmodel(model)
 
         
