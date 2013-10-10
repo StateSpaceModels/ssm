f69074db89daee393e8bbd306672b652310b3966
JDureau <dureau.joseph@gmail.com>
2013-10-10 15:37:12 +0200

    models moved

--- ssm/test_Ccoder.py
+++ ssm/test_Ccoder.py
@@ -8,51 +8,51 @@ class TestCcoder(unittest.TestCase):
 
     def setUp(self):
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         self.m_noise = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'diff', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','diff', 'model', 'datapackage.json')))
         self.m_diff = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         del model["resources"][1]["data"][2]["white_noise"] 
         del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][0]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][1]["white_noise"] = {"id":"noise_SI2", "sd": "sto"}
         self.m_noise2 = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         del model["resources"][1]["data"][2]["white_noise"] 
         del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][4]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][5]["white_noise"] = {"id":"noise_SI2", "sd": "sto"}
         self.m_noise3 = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         del model["resources"][1]["data"][2]["white_noise"] 
         del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][8]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][9]["white_noise"] = {"id":"noise_SI2", "sd": "sto"}
         self.m_noise4 = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         del model["resources"][1]["data"][2]["white_noise"] 
         del model["resources"][1]["data"][3]["white_noise"]
         model["resources"][1]["data"][10]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][11]["white_noise"] = {"id":"noise_SI2", "sd": "sto"}
         self.m_noise5 = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         model["resources"][1]["data"][4]["white_noise"] = {"id":"noise_SI", "sd": "sto"}
         model["resources"][1]["data"][5]["white_noise"] = {"id":"noise_SI2", "sd": "sto"}
         self.m_noise6 = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'noise', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','noise', 'model', 'datapackage.json')))
         model["resources"][1]["data"][4]["white_noise"] = {"id":"noise_SI23", "sd": "sto"}
         model["resources"][1]["data"][5]["white_noise"] = {"id":"noise_SI24", "sd": "sto"}
         self.m_noise7 = Ccoder(model)
 
-        model = json.load(open(os.path.join('..' ,'diff', 'model', 'datapackage.json')))
+        model = json.load(open(os.path.join('..', 'example','diff', 'model', 'datapackage.json')))
         model["resources"][1]["data"].append({"from": "R_paris",   "to": "I_paris",   "rate": "correct_rate(v)",            "description":"testing"})
         model["resources"][1]["data"].append({"from": "R_nyc",   "to": "I_nyc",   "rate": "correct_rate(v)",                "description":"testing"})
         self.m_diff2 = Ccoder(model)
