d4397e03fe77801a764939aa079677145cfa9918
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-10-10 11:20:34 -0400

    test var

--- tests/input.c
+++ tests/input.c
@@ -5,6 +5,7 @@ static json_t *jparameters;
 static ssm_nav_t *nav;
 static ssm_options_t *opts;
 static ssm_input_t *input;
+static ssm_var_t *var;
 
 void test_input__initialize(void)
 {
@@ -13,6 +14,7 @@ void test_input__initialize(void)
     nav = ssm_nav_new(jparameters, opts);
 
     input = ssm_input_new(jparameters, nav);
+    var = ssm_var_new(jparameters, nav);
 }
 
 void test_input__cleanup(void)
@@ -22,13 +24,12 @@ void test_input__cleanup(void)
     ssm_nav_free(nav);
 
     ssm_input_free(input);
+    ssm_var_free(var);
 }
-
 
 void test_input__input_new(void)
 {        
     int i;
-
     double expected[] = {
 	1e-05, //I_nyc
 	1e-05, //I_paris
@@ -49,4 +50,30 @@ void test_input__input_new(void)
     for(i=0; i<nav->par_all->length; i++){
 	cl_check(gsl_vector_get(input, nav->par_all->p[i]->offset) == expected[i]);
     }
+
+}
+
+
+void test_input__var_new(void)
+{        
+    int i, j;
+    double expected[10][10] = {
+	{0.01, 0,    0,    0,    0,    0,    0,    0,    0,    0},
+	{0,    0.03, 0,    0,    0,    0,    0,    0,    0,    0},
+	{0,    0,    0.02, 0,    0,    0,    0,    0,    0,    0},
+	{0,    0,    0,    0.04, 0,    0,    0,    0,    0,    0},
+	{0,    0,    0,    0,    0.02, 0,    0,    0,    0,    0},
+	{0,    0,    0,    0,    0,    0.02, 0,    0,    0,    0},
+	{0,    0,    0,    0,    0,    0,    0.02, 0,    0,    0},
+	{0,    0,    0,    0,    0,    0,    0,    0.02, 0,    0},
+	{0,    0,    0,    0,    0,    0,    0,    0,    0.02, 0},
+	{0,    0,    0,    0,    0,    0,    0,    0,    0,    0.03}
+    };
+
+    for(i=0; i<nav->theta_all->length; i++){
+	for(j=0; j<nav->theta_all->length; j++){
+	    cl_check(gsl_matrix_get(var, nav->theta_all->p[i]->offset_theta, nav->theta_all->p[j]->offset_theta) == expected[i][j]);
+	}
+    }
+
 }
