aaf02fcb46308da40702e5beb3bc9d8a46fcf326
JDureau <dureau.joseph@gmail.com>
2013-10-14 09:44:27 +0200

    f diff initialised in step_ekf

--- ssm/src/C/templates/ode_sde_template.c
+++ ssm/src/C/templates/ode_sde_template.c
@@ -53,8 +53,8 @@ void ssm_step_sde_{{ noises_off }}(ssm_X_t *p_X, double t, ssm_par_t *par, ssm_n
         }
         {% else %}
         diffed[i] = gsl_vector_get(par, p->ic->offset);
-	f[p->offset]=0;
         {% endif %}
+	f[p->offset]=0;
     }
     {% endif %}
 
