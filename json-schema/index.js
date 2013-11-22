dd8d385a04ca24304416b1e2119868c930ca082f
JDureau <dureau.joseph@gmail.com>
2013-11-22 16:09:31 +0100

    validate.js

--- json-schema/index.js
+++ json-schema/index.js
@@ -5,16 +5,18 @@ var noise = require('./noise_example.json');
 var foo = require('./foo_example.json');
 var schema = require('./schema.json');
 
-var valid = tv4.validate(noise, schema);
-if (valid){
-  console.log(valid);
-} else {
-  console.log(tv4.error);
+function Validate(str){
+  /**
+   * check that package.json is a proper JSON file
+   */
+  
+  var valid = tv4.validate(noise, schema);
+  if (valid){
+    console.log(valid);
+  } else {
+    console.log(tv4.error);
+  }
 }
 
-var valid = tv4.validate(foo, schema);
-if (valid){
-  console.log(valid);
-} else {
-  console.log(tv4.error);
-}
+Validate({"a":"1});
+
