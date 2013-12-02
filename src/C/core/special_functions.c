8d66a28c40d5528fbb6ba5b23cf4428d9262b076
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-02 14:42:40 -0500

    special functions

---
+++ src/C/core/special_functions.c
@@ -0,0 +1,48 @@
+/**************************************************************************
+ *    This file is part of ssm.
+ *
+ *    ssm is free software: you can redistribute it and/or modify it
+ *    under the terms of the GNU General Public License as published
+ *    by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    ssm is distributed in the hope that it will be useful, but
+ *    WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public
+ *    License along with ssm.  If not, see
+ *    <http://www.gnu.org/licenses/>.
+ *************************************************************************/
+
+#include "ssm.h"
+
+/**
+ * The Heaviside step function or unit step function
+ * x is typically t-t_intervention
+ */
+double heaviside(double x)
+{    
+    return (x < 0.0) ? 0.0 : 1.0;
+}
+
+/**
+ * The ramp function 
+ */
+double ramp(double x)
+{
+    return (x >= 0) ? x : 0.0;
+}
+
+/**
+ * Slowstep function : piecewise linear function equivalent to
+ * integral of a rectangular function. Of the form
+ * slowstep(x, d) = {   0     if x < 0
+ *                      d     if x >= d
+ *                      x     otherwise   }
+ */
+double slowstep(double x, double d)
+{
+    return ( x >= 0.0 ? ( x >= d ? d : x ) : 0.0 );
+}
